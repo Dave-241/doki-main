9546ad537fa1d85c0d8eae5a50b49153e7460d48
DAVIS ISIBOR <davisisibor@gmail.com>
2023-01-09 18:32:58 +0100

    x

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app'
 import "../styles/home.css"
+import "../../styles/hero.css"
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- pages/components/hero.tsx
+++ pages/components/hero.tsx
@@ -1,4 +1,3 @@
-import "../../styles/hero.css"
 
 const Hero = () => {
 
